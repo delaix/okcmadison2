587c0164c71294ab1108b1ea8f356b1e1ee20e12
Andrew A. de Laix <delaix@wolfram.com>
2020-03-29 14:58:56 -0500

    working version of page under latest rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "pages/home"
+  get "pages/about"
+  get "pages/classes"
+  get "pages/blog"
+  get "pages/practical"
+  
+  root :to => 'pages#home'
 end
